c76582b10f6a5498c19b26d882ef50e1097f9160
sookyeongyeom <sookyeong.dev@gmail.com>
2022-08-03 08:28:10 +0900

    :books: p195

---
+++ src/Info.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+
+const Info = () => {
+  const [name, setName] = useState('');
+  const [nickname, setNickname] = useState('');
+  useEffect(() => {
+    console.log('렌더링 완료! (마운트 때만 실행됨)');
+    console.log({
+      name,
+      nickname,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // 최초 렌더링 시에만 실행하려면 두 번째 파라미터로 빈 배열을 넣어줌
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <div>
+          <b>이름 : </b> {name}
+        </div>
+        <div>
+          <b>닉네임 : </b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
